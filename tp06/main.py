ee3fa985e893233d35b89c185972523fc2d8d2b1
Frédéric Gaurat <fgaurat@eolem.com>
2022-04-27 14:23:22 +0200

    add @contextlib.contextmanager

--- tp06/main.py
+++ tp06/main.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python
 
+import contextlib
 from CustomerDAO import CustomerDAO
+
+@contextlib.contextmanager
+def a_function(filename):
+    file = open(filename)
+    print("enter")
+    try:
+        yield file
+    finally:
+        print("exit")
+        file.close()
 
 
 def main():
+    with a_function('data.csv') as f:
+        for line in f:
+            print(line.strip())
+
+    print(f.closed)
+
     
 
+
+
+def main02():
+    
     customerDAO = CustomerDAO('data.csv')
 
     print("start context")
